e80b90b05afb12dcbad78d20a7a7bd12e876434c
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-03-11 17:12:20 -0500

    did some stuff

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,17 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  console.log("YOYOYOYIPPIYOYIPPYYAYWHERE MY STARS AT ********************************************")
+  console.log(item.enhancement)
+  newItem = {
+    name: item.name,
+    durability: item.durability,
+    enhancement: item.enhancement += 1 
+  }
+  if (newItem.enhancement > 20) {
+    return item
+  } 
+  return  newItem ;
 }
 
 function fail(item) {
@@ -14,9 +24,22 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  }
+  return newItem;
 }
 
 function get(item) {
   return { ...item };
 }
+
+const item = {
+  name :"Lambda Shield",
+  durability : 87,
+  enhancement : 2
+}
+
+succeed(item);
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,26 @@
 const enhancer = require('./enhancer.js');
 // test away!
+it("Repair increases durability to 100", () => {
 
-expect(enhancer.repair(item).durability).toBe(100);
+    const item = {
+        name :"Lambda Shield",
+        durability : 87,
+        enhancement : 2
+      }
+      expect(enhancer.repair(item).durability).toBe(100);
+})
+
+it("Succeed returns back an item that has been enhanced", () => {
+
+    const item = {
+        name :"Lambda Shield",
+        durability : 87,
+        enhancement : 2
+      }
+      const prevItem = {
+          name: item.name,
+          durability: item.durability,
+          enhancement: item.enhancement -= 1
+      }
+      expect(enhancer.succeed(item).enhancement).toEqual(prevItem.enhancement + 1);
+})
